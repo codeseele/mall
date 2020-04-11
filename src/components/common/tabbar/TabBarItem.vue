1a6c12a72583fa18a1e6e59d97f8c2be5b033524
codeseele <673608135@qq.com>
2020-04-11 16:53:09 +0800

    new file

---
+++ src/components/common/tabbar/TabBarItem.vue
@@ -0,0 +1,70 @@
+<template>
+
+  <div
+    @click="itemClick"
+    class="tab-bar-item"
+  >
+    <div v-if="!isActive">
+      <slot name="item-icon"></slot>
+    </div>
+    <div v-else>
+      <slot name="item-icon-active"></slot>
+    </div>
+    <div :style="activeStyle">
+      <slot name="item-text"></slot>
+    </div>
+
+    <!-- <img src="../../assets/img/tabbar/home.svg" alt="">
+      <div>首页</div> -->
+
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'TabBarItem',
+  props: {
+    path: String,
+    activeColor: {
+      type: String,
+      default: 'red'
+    }
+  },
+  data() {
+    return {
+      // isActive:true
+    }
+  },
+  computed: {
+    isActive() {
+      return this.$route.path.indexOf(this.path) !== -1
+    },
+    activeStyle() {
+      return this.isActive ? { color: this.activeColor } : {}
+    }
+  },
+  methods: {
+    itemClick() {
+      this.$router.push(this.path).catch(data => { })
+    }
+  },
+}
+</script>
+
+<style scoped>
+.tab-bar-item {
+  flex: 1;
+  text-align: center;
+  height: 49px;
+  font-size: 14px;
+}
+
+.tab-bar-item img {
+  width: 24px;
+  height: 24px;
+  margin-top: 3px;
+  vertical-align: middle;
+  margin-bottom: 2px;
+}
+</style>
