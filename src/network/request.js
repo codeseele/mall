de4a2cea5481fb82ca6c52ab384991a93d597d8e
codeseele <673608135@qq.com>
2020-04-12 00:18:17 +0800

    commit message

---
+++ src/network/request.js
@@ -0,0 +1,23 @@
+import axios from 'axios'
+
+
+export function request(config) {
+  // 1.创建axios实例
+  const instance = axios.create({
+    baseURL: 'http://123.207.32.32:8000',
+    timeout: 5000
+  })
+
+  // 2.axios的拦截器
+    instance.interceptors.request.use(config => {
+      console.log(config);
+      return config
+    }, err => {
+      console.log(err);
+    })
+
+    axios.interceptors.response.use()
+
+  // 3.发送真正的网络请求
+  return instance(config)
+}
